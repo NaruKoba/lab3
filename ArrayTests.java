7b516529dfadc2c531e1b908bcbf661149f3d69e
NaruKoba <101598791+NaruKoba@users.noreply.github.com>
2023-11-05 00:44:52 -0700

    Fixed ArrayExamples

--- ArrayTests.java
+++ ArrayTests.java
@@ -7,12 +7,21 @@ public class ArrayTests {
     int[] input1 = { 3 };
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
+
+    int[] input2 = { 3, 6 , 5, 7 };
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[]{ 7, 5, 6, 3 }, input2);
 	}
+
+
 
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+
+    int[] input2 = { 3, 6 , 5, 7 };
+    assertArrayEquals(new int[]{ 7, 5, 6, 3 }, ArrayExamples.reversed(input2));
   }
 }
